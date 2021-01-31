ae20b4a4608f1514e66b5ad0ad13ce6272bbcdbf
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-01-31 23:45:34 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,11 @@
 def countdown(number)
-    loop while number > 0
+    while number > 0
     puts ("#{number} SECOND(S)!")
     number -= 1
 end
-
-  puts ("HAPPY NEW YEAR!")
+end
+when number = 0
+    puts ("HAPPY NEW YEAR!")
 end
 
 
