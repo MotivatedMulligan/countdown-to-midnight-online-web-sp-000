9e2d4fa08502824ab721f6c94662e0bebd465dc7
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-01-31 23:51:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,9 @@
 def countdown(number)
-    if number > 0
+while number > 0
     puts ("#{number} SECOND(S)!")
     number -= 1
-  elsif
-      puts ("HAPPY NEW YEAR!")
-
 end
+return "HAPPY NEW YEAR!"
 end
 
 
